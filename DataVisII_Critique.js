75104b965d042d6ba0676153793d3546ffce2091
agur0011 <161534441+agur0011@users.noreply.github.com>
2024-10-07 14:03:55 +1100

    Create DataVisII_Critique.js

---
+++ DataVisII_Critique.js
@@ -0,0 +1,10 @@
+var vg_1 = "VegaLite Code (MAP).json";
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "Scatterplot - Week 10 HW.json";
+vegaEmbed("#scatterplot", vg_2).then(function(result) {
+
+}).catch(console.error);
+
