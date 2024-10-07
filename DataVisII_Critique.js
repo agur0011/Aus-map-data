bade0835c55aa98df7f08d6ba0afc5684a93d600
agur0011 <161534441+agur0011@users.noreply.github.com>
2024-10-07 14:14:36 +1100

    Update DataVisII_Critique.js

--- DataVisII_Critique.js
+++ DataVisII_Critique.js
@@ -1,14 +1,14 @@
-var vg_1 = "VegaLite Code (MAP).json";
+var vg_1 = "./VegaLite Code (MAP).json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 
 }).catch(console.error);
 
-var vg_2 = "Scatterplot - Week 10 HW.json";
+var vg_2 = "./Scatterplot - Week 10 HW.json";
 vegaEmbed("#scatterplot", vg_2).then(function(result) {
 
 }).catch(console.error);
 
-var vg_3 = "Pie chart code.json";
+var vg_3 = "./Pie chart code.json";
 vegaEmbed("#piechart", vg_3).then(function(result) {
 
 }).catch(console.error);
