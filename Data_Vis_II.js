1c753f7e44df9deac027b0f915f00aed21f1bc0a
agur0011 <161534441+agur0011@users.noreply.github.com>
2024-10-10 22:20:47 +1100

    Update Data_Vis_II.js

--- Data_Vis_II.js
+++ Data_Vis_II.js
@@ -18,7 +18,7 @@ vegaEmbed("#barchart1", vg_4).then(function(result) {
 
 }).catch(console.error);
 
-var vg5 = "./Deposit_bar.json";
+var vg_5 = "./Deposit_bar.json";
 vegaEmbed("#barchart2", vg_5).then(function(result) {
 
 }).catch(console.error);
