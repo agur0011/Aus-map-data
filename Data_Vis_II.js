ec0bdc20bcecef80a72568ebe0f505a51a5a41e0
agur0011 <161534441+agur0011@users.noreply.github.com>
2024-10-10 22:29:04 +1100

    Create Data_Vis_II.js

---
+++ Data_Vis_II.js
@@ -0,0 +1,24 @@
+var vg_1 = "./VegaLite Code (MAP).json";
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "./Scatterplot - Week 10 HW.json";
+vegaEmbed("#scatterplot", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "./Pie_chart_code.json";
+vegaEmbed("#piechart", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "./Bar_chart.json";
+vegaEmbed("#barchart1", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "./Deposit_bar.json";
+vegaEmbed("#deposit_chart", vg_5).then(function(result) {
+
+}).catch(console.error);
