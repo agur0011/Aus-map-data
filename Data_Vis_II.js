5f2224f7b6321ae3ea31a35cfd9e8dde66e788f6
agur0011 <161534441+agur0011@users.noreply.github.com>
2024-10-14 22:27:05 +1100

    Update Data_Vis_II.js

--- Data_Vis_II.js
+++ Data_Vis_II.js
@@ -3,7 +3,7 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 
 }).catch(console.error);
 
-var vg_2 = "./Scatterplot_Wk10.json";
+var vg_2 = "./ScatterplotWk10.json";
 vegaEmbed("#scatterplot", vg_2).then(function(result) {
 
 }).catch(console.error);
